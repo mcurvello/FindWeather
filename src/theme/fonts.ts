c7c796d8e77588a0f92c8042ccf28a9270c73fdf
Marcio Curvello <marciocurvello@gmail.com>
2023-03-11 12:15:36 -0300

    feat: add fonts

--- src/theme/fonts.ts
+++ src/theme/fonts.ts
@@ -3,8 +3,13 @@ import { FontSource } from "expo-font";
 
 export const loadFonts = async () => {
   await Font.loadAsync({
-    "Overpass-Regular":
+    Overpass_300Regular:
+      require("../assets/fonts/Overpass-Light.ttf") as FontSource,
+    Overpass_400Regular:
       require("../assets/fonts/Overpass-Regular.ttf") as FontSource,
-    "Overpass-Bold": require("../assets/fonts/Overpass-Bold.ttf") as FontSource,
+    Overpass_600SemiBold:
+      require("../assets/fonts/Overpass-SemiBold.ttf") as FontSource,
+    Overpass_700Bold:
+      require("../assets/fonts/Overpass-Bold.ttf") as FontSource,
   });
 };
